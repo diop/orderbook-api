ec2033dc25234d1e5fa03fc8c98a6adbe27b74e7
diop <fodesdiop@gmail.com>
2018-12-09 23:51:33 -0800

    update order schema

--- src/server/models/order.model.js
+++ src/server/models/order.model.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 const OrderSchema = mongoose.Schema({
-    token_name: String,
-    price: String
+    exchangeAddress: String,
+    senderAddress: String,
+    makerAssetData: String,
+    takerAssetData: String,
+    traderAssetData: String,
+    makerAddress: String,
+    takerAddress: String,
+    traderAddress: String,
+    feeRecipientAddress: String
 }, {
     timestamps: true
 })
