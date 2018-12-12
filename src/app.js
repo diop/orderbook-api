cd296fc1bb1eed74bf5afbb2db457b582efe60a1
diop <fodesdiop@gmail.com>
2018-12-12 01:19:46 -0800

    wip

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 3000
 const morgan = require('morgan')
 
@@ -19,11 +22,27 @@ mongoose.connect(dbConfig.url, {
     process.exit()
 })
 
+app.use(cookieParser())
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (request, response) => response.send({message: `Trustless Token Orderbook API Endpoints`}))
 
+var checkAuth = (request, response, next) => {
+    if (typeof request.cookies.nToken === 'undefined' || request.cookies.nToken === null) {
+        request.user = null
+    } else {
+        const token = request.cookies.nToken
+        const decodedToken = jwt.decode(token, { complete: true }) || {}
+        request.user = decodedToken.payload
+    }
+    next()
+}
+
+app.use(checkAuth)
+
 require('./app/controllers/sessions_controller')(app)
 require('./config/routes/orders_router')(app)
+
 app.listen(port, () => console.log(`Webserver is running on ${port}...`))
--- src/app/controllers/sessions_controller.js
+++ src/app/controllers/sessions_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const Order = require('../models/order.js')
 
 module.exports = app => {
     app.get('/sign-up', (request, response ) => {
@@ -16,8 +17,9 @@ module.exports = app => {
     })
 
     app.post('/sign-up', (request, response) => {
+        console.log(request.body)
         const user = new User(request.body)
-
+        
         user
             .save()
             .then(user => {
@@ -38,12 +40,12 @@ module.exports = app => {
         User.findOne({ username }, 'username password')
             .then(user => {
                 if (!user) {
-                    return response.status(401).send({ message: 'Wrong username or password' })
+                    return response.status(401).send({ message: 'Wrong username or password1' })
                 }
 
                 user.comparePassword(password, (err, isMatch) => {
                     if (!isMatch) {
-                        return response.satus(401).send({ message: 'Wrong username or password' })
+                        return response.status(401).send({ message: 'Wrong username or password2' })
                     }
 
                     const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { 
@@ -51,11 +53,40 @@ module.exports = app => {
                     })
                     
                     response.cookie('nToken', token, { maxAge: 900000, httpOnly: true })
-                    response.redirect('/')
+                    response.redirect(`/`)
                 })
             })
             .catch(error => {
                 console.log(error)
+            })
+    })
+
+    app.post('/orders/new', (request, response) => {
+        if (request.user) {
+            const order = new Order(request.body)
+
+            order
+                .save()
+                .then(order => {
+                    response.redirect(`/orders/` + order._id)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        } else {
+            return response.status(401) // Unauthorized
+        }
+    })
+
+    app.get('/orders/:id', (request, response) => {
+        const id = request.params.id
+        Order
+            .findById(id)
+            .then(order => {
+                response.redirect(`/orders/` + order._id)
+            })
+            .catch(error => {
+                console.log(error.message)
             })
     })
 }
